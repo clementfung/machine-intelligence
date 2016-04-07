e183fbcd4fd0371fbfd862cb6931e820fc7c0170
Chris Ngan <chris.ngan24@gmail.com>
2016-04-07 17:59:03 -0400

    Added feature factory

--- feature_eng.py
+++ feature_eng.py
@@ -1,9 +1,11 @@
+
 """
 Set of rule based features
 """
 import abc
 
 import cleaner
+import pandas as pd
 
 
 #################
@@ -30,7 +32,7 @@ class FeatureGenerator:
         pass
 
     def get_feature_name(self):
-        return self.get_feature_name
+        return self.__class__.__name__
 
     def get_feature_description(self):
         return self.feature_description
@@ -39,16 +41,14 @@ class FeatureGenerator:
     @abc.abstractmethod
     def apply_rules(self, row):
         pass
+
 class SearchTermMatch(FeatureGenerator):
-    feature_name = 'feat_search_term'
     feature_description = 'Is the search term in the product title?'
 
     def apply_rules(self, row):
         searh_term = tokenize_string(row['search_term'])
         prod_title = tokenize_string(row['product_title'])
         return prod_title.find(searh_term) != - 1
-
-    
 
 
 
@@ -60,7 +60,7 @@ class SearchTermMatch(FeatureGenerator):
 #####
 class FeatureFactory:
     def __init__(self):
-        pass
+        self.feature_generators = map(lambda x: x(), self.feature_classes())
 
     def feature_classes(self):
         """
@@ -71,11 +71,23 @@ class FeatureFactory:
                 ]
 
 
+    def get_feature_names(self):
+        return map(lambda x: x.get_feature_name(), self.feature_generators)
+
+    def apply_feature_eng(self,df):
+
+        for feat in self.feature_generators:
+            df[feat.get_feature_name()] = df.apply(
+                    feat.apply_rules, axis=1
+                    )
+        return df
+
 if __name__ == '__main__':
     st = SearchTermMatch()
     ff = FeatureFactory()
 
     # show that it actually creates objects
-    map(lambda x: x().get_feature_name(), ff.feature_classes())
+    print ff.get_feature_names()
     df = pd.read_csv('data/train_sample.csv')
+    df2 = ff.apply_feature_eng(df)
     import pdb; pdb.set_trace()
